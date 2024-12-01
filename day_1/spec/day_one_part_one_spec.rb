076bff14be1552d38230d792977c1c0dd3cb49d6
Gary Y <garyyau080@gmail.com>
2024-12-02 00:10:41 +0900

    test: Add simple test with test input

---
+++ day_1/spec/day_one_part_one_spec.rb
@@ -0,0 +1,10 @@
+require 'day_one_part_one.rb'
+
+RSpec.describe DayOnePartOne do
+  describe '#solve' do
+    it 'solves day one part one' do
+      aoc = DayOnePartOne.new("../spec/test_input.txt")
+      expect(aoc.solve).to eq(11)
+    end
+  end
+end
---
+++ day_1/spec/day_one_part_two_spec.rb
@@ -0,0 +1,10 @@
+require 'day_one_part_two.rb'
+
+RSpec.describe DayOnePartTwo do
+  describe '#solve' do
+    it 'solves day one part two' do
+      aoc = DayOnePartTwo.new("../spec/test_input.txt")
+      expect(aoc.solve).to eq(31)
+    end
+  end
+end
